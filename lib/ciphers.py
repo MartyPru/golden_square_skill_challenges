bf6bef932e3413fa25916bb39dc6e4176ba5a17f
Martynas Prusevicius <marty@MASTUDENT-Martynas-Prusevicius-JGF21QNJTW.local>
2023-11-14 11:29:09 +0000

    Complete exercise 3 in Debugging 1

--- lib/ciphers.py
+++ lib/ciphers.py
@@ -11,24 +11,22 @@ def encode(text, key):
 
 def decode(encrypted, key):
     cipher = make_cipher(key)
-
     plaintext_chars = []
     for i in encrypted:
-        plain_char = cipher[65 - ord(i)]
+        plain_char = cipher[ord(i) - 65]
         plaintext_chars.append(plain_char)
-
     return "".join(plaintext_chars)
 
 
 def make_cipher(key):
-    alphabet = [chr(i + 98) for i in range(1, 26)]
+    alphabet = [chr(i + 96) for i in range(1, 27)]
+
     cipher_with_duplicates = list(key) + alphabet
 
     cipher = []
     for i in range(0, len(cipher_with_duplicates)):
         if cipher_with_duplicates[i] not in cipher_with_duplicates[:i]:
             cipher.append(cipher_with_duplicates[i])
-    print(cipher)
     return cipher
 
 # When you run this file, these next lines will show you the expected
